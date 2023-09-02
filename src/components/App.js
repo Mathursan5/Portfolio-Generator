2428d12f96df518448aec15f9c19c15d6f5159f6
mathursanmathu13@gmail.com <mathursanmathu13@gmail.com>
2023-09-02 16:07:10 +0530

    added header

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import Header from './Bootstrap/Header'
 
 class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <Header>My Portfolio Generator</Header>
     )
   }
 }
