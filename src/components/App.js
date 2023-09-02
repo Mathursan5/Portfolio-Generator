b196bd70d1a48d915d4cf132a3df40ffa85f4596
mathursanmathu13@gmail.com <mathursanmathu13@gmail.com>
2023-09-02 20:28:03 +0530

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react'
 import Header from './Bootstrap/Header'
 
 class App extends Component {
+  state = {
+    Dark: false
+  };
   render() {
     return (
-      <Header>My Portfolio Generator</Header>
+      // <Header dark={true}>My Portfolio Generator Lite</Header>
+      <Header dark={this.state.Dark}> My Portfolio Generator Dark</Header>
     )
   }
 }
